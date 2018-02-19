2c0c7c02a7dd015b2820a7bf7c5e49aa1f0dd611
panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2018-02-18 17:00:05 -0800

    getting all the fields in pg

--- types/me.js
+++ types/me.js
@@ -3,12 +3,17 @@ const {
   GraphQLNonNull,
   GraphQLID,
   GraphQLString
-} = require('graphql');
+} = require('graphql')
+
+const { fromSnakeCase } = require('../lib/util');
 
 module.exports = new GraphQLObjectType( {
   name: 'MeType',
   fields: {
     id: { type:GraphQLID },
+    firstName: fromSnakeCase(GraphQLString),
+    lastName: fromSnakeCase(GraphQLString),
+    createdAt: fromSnakeCase(GraphQLString),
     email: { type : new GraphQLNonNull(GraphQLString)}
   }
 })
