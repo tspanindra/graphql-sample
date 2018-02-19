ade86b5af8dcb791821d803ef7069df18ac9304f
panindra Tumkur Seetharamu <ptumkurseetharamu@godaddy.com>
2018-02-18 20:15:14 -0800

    hooked up mongodb driver and getting count data from mongoose

--- database/pgdb.js
+++ database/pgdb.js
@@ -1,3 +1,5 @@
+const humps = require('humps');
+ 
 module.exports = pgPool => {
   return {
     getUser(apiKey) {
@@ -5,8 +7,17 @@ module.exports = pgPool => {
         select * from users 
         where api_key = $1
       `, [apiKey]).then(res => {
-        return res.rows[0];
+        return humps.camelizeKeys(res.rows[0]);
       });
+    },
+
+    getContests(user) {
+      return pgPool.query(`
+        select * from contests
+        where created_by = $1
+      `, [user.id]).then(res => {
+          return humps.camelizeKeys(res.rows);
+        });
     }
   }
 }
--- types/me.js
+++ types/me.js
@@ -2,18 +2,48 @@ const {
   GraphQLObjectType,
   GraphQLNonNull,
   GraphQLID,
-  GraphQLString
+  GraphQLString,
+  GraphQLList,
+  GraphQLInt
 } = require('graphql')
 
 const { fromSnakeCase } = require('../lib/util');
+const ContestsType = require('./contests');
+const pgdb = require('../database/pgdb');
+const mdb = require('../database/mdb');
 
 module.exports = new GraphQLObjectType( {
   name: 'MeType',
   fields: {
     id: { type:GraphQLID },
-    firstName: fromSnakeCase(GraphQLString),
-    lastName: fromSnakeCase(GraphQLString),
-    createdAt: fromSnakeCase(GraphQLString),
-    email: { type : new GraphQLNonNull(GraphQLString)}
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    createdAt: { type: GraphQLString },
+    email: { type : new GraphQLNonNull(GraphQLString)},
+    contests: {
+      type: new GraphQLList(ContestsType),
+      resolve(obj, args, { pgPool }) {
+        // Read contests from db
+        return pgdb(pgPool).getContests(obj);
+      }
+    },
+    contestsCount: {
+      type: GraphQLInt,
+      resolve(obj, args, { mPool }, { fieldName}) {
+        return mdb(mPool).getCounts(obj, fieldName)
+      }
+    },
+    namesCount: {
+      type: GraphQLInt,
+      resolve(obj, args, { mPool }, { fieldName}) {
+        return mdb(mPool).getCounts(obj, fieldName)
+      }
+    },
+    votesCount: {
+      type: GraphQLInt,
+      resolve(obj, args, { mPool }, { fieldName}) {
+        return mdb(mPool).getCounts(obj, fieldName)
+      }
+    }
   }
 })
